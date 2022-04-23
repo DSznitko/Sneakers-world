b8c79017ca11853300d878920abeef4303bedfe2
Daniel Sznitko <93942736+DSznitko@users.noreply.github.com>
2022-04-23 21:31:42 +0200

    download bundlers

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,2 @@
 "use strict";var test=function(){console.log("test")};
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6WyJ0ZXN0IiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6ImFBQUEsSUFBTUEsS0FBTyxXQUFiQyxRQUFVQyxJQUFHIiwiZmlsZSI6InNjcmlwdC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnN0IHRlc3QgPSAoKSA9PiB7XHJcblxyXG4gIGNvbnNvbGUubG9nKFwidGVzdFwiKVxyXG59XHJcbiJdfQ==
--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,16 @@ const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 function sassCompiler(done) {
   src('./src/sass/**/*.scss')
+  .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(cssnano())
+    .pipe(sourcemaps.write())
     .pipe(dest('./dist/css'));
 
     done()
@@ -19,14 +22,25 @@ function sassCompiler(done) {
 
 function javaScript(done) {
   src('./src/js/**/*.js')
+  .pipe(sourcemaps.init())
   .pipe(babel({
       presets: ['@babel/env']
   }))
   .pipe(uglify())
+  .pipe(sourcemaps.write())
   .pipe(dest('./dist/js'))
 
 
   done()
 }
 
-exports.default = series(sassCompiler, javaScript)
+function converteImgs(done) {
+  src('./src/img/*')
+ .pipe(imagemin())
+  .pipe(dest('./dist/img'))
+
+
+  done()
+}
+
+exports.default = series(sassCompiler, javaScript, converteImgs)
